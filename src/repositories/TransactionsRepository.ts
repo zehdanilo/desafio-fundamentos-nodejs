02bb3112ca309b17164e6f2ac263b23fe0173c26
Danilo Diógenes <zehdanilo@gmail.com>
2020-04-29 20:43:12 -0300

    Desafio concluido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    // const projects.filter(project => project.title.includes(title))
+
+    const transactionsIncome = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+
+    const transactionsOutCome = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
+
+    const sumOfIncome = transactionsIncome.reduce((sum, transaction) => {
+      return sum + transaction.value;
+    }, 0);
+
+    const sumOfOutcome = transactionsOutCome.reduce((sum, transaction) => {
+      return sum + transaction.value;
+    }, 0);
+
+    const balance = {
+      income: sumOfIncome,
+      outcome: sumOfOutcome,
+      total: sumOfIncome - sumOfOutcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
